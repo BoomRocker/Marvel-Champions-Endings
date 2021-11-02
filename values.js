d3d692bfca8de2fc2161175d6725dbe56514af97
BoomRocker <boomrocker@gmail.com>
2021-11-02 15:16:24 -0500

    Updates
    Updates

--- values.js
+++ values.js
@@ -1,82 +1,50 @@
-function getInputValue(){
-    // Selecting the input element and get its value
-    //var hero = document.getElementById("myHero").value;
-    //var villain = document.getElementById("theVillain").value;
-    // Displaying the value
-    //alert(hero + "  " + villain);
-}
+
+/////////////////////////////////////
 
 function start()
 {
-  var hero = document.getElementById("myHero").value;
-  var villain = document.getElementById("theVillain").value;
-
-  var heroes = [
-    "Spiderman",
-    "Thor",
-    "Captain America",
-    "Iron Man" ];
-
-  var article = [
-    "the",
-    "a",
-    "one",
-    "some",
-    "any" ];
+	var hero = document.getElementById("myHero").value;
+	
+	var villain = document.getElementById("theVillain").value;
+	
+	var article = ["the","a","one","some","any" ];
+	
+	var preposition = [ "to","from","over","under","on" ];
+	
+	var fallflavor = [ "falls to the ground with a great THUD.", "crashes into an armored truck, with a fantastic CRASH" ];
     
 // Hero Spiderman Conditionals
-  if (hero == "Spiderman") {
-    var verb = [
-      "flipped",
-      "jumped",
-      "spidey-sensed",
-      "stuck",
-      "th-wipped" ];
-    }
+  if (hero == "Spiderman") { var verb = ["flipped","jumped","spidey-sensed","stuck","th-wipped" ]; }
 
 // Villain Rhino Conditionals
-  if (villain == "Rhino") {
-    var noun = [
-      "horn",
-      "tusk",
-      "claw",
-      "skin",
-      "teeth" ];
-    }
+  if (villain == "Rhino") { 
+	var noun = [ "horn", "tusk", "claw", "skin","teeth" ]; 
+	var verb = [ "skulked", "lumbered", "stumbled", "trampled","thudded" ]; 
+	}
 
 // Villian Green Goblin Conditionals
-  if (villain == "Green Goblin") {
-      var noun = [
-        "pumpkin bombs",
-        "devil sled",
-        "mutagen",
-        "explosive",
-        "lasers" ];
-      }
+  if (villain == "Green Goblin") { 
+	var noun = [ "pumpkin bombs","devil sled","mutagen","explosive","lasers" ];	
+	var verb = [ "thrust", "skimpered", "slinked", "slid","splat" ]; 
+	}
+	
+	endingHeader.innerHTML = "Congratulations " + hero + ", you've defeated " + villain + " !\n";
 
-  var preposition = [
-    "to",
-    "from",
-    "over",
-    "under",
-    "on" ];
-
-  var story = document.getElementById( "ending" );
-    ending.innerHTML = "Congratulations " + hero + ", you've defeated " + villain + "\n";
-
-  for (var i = 0; i < 10; i++)
-  {
-    ending.innerHTML += capitalizeFirstLetter(article[Math.floor(Math.random() * article.length) ] ) + " ";
-    ending.innerHTML += hero + " ";
-    ending.innerHTML += noun[Math.floor(Math.random() * noun.length)] + " ";
-    ending.innerHTML += verb[Math.floor(Math.random() * verb.length)] + " ";
-    ending.innerHTML += article[Math.floor(Math.random() * article.length)] + " ";
-    ending.innerHTML += noun[Math.floor(Math.random() * noun.length)] + " ";
-    ending.innerHTML += preposition[Math.floor(Math.random() * preposition.length)] + " ";
-    ending.innerHTML += article[Math.floor(Math.random() * article.length)] + " ";
-    ending.innerHTML += noun[Math.floor(Math.random() * noun.length)] + ". ";
-
-  }
+  var story = document.getElementById( "storyContent" );
+  	ending.innerHTML = "\n";
+	  for (var i = 0; i < 5; i++)
+	  {
+		//ending.innerHTML += capitalizeFirstLetter(article[Math.floor(Math.random() * article.length) ] ) + " ";
+		ending.innerHTML += villain + " ";
+		//ending.innerHTML += noun[Math.floor(Math.random() * noun.length)] + " ";
+		ending.innerHTML += fallflavor[Math.floor(Math.random() * fallflavor.length)] + " ";
+		ending.innerHTML += verb[Math.floor(Math.random() * verb.length)] + " ";
+		ending.innerHTML += article[Math.floor(Math.random() * article.length)] + " ";
+		ending.innerHTML += noun[Math.floor(Math.random() * noun.length)] + " ";
+		ending.innerHTML += preposition[Math.floor(Math.random() * preposition.length)] + " ";
+		ending.innerHTML += article[Math.floor(Math.random() * article.length)] + " ";
+		ending.innerHTML += noun[Math.floor(Math.random() * noun.length)] + ". ";
+	  }
   ending.innerHTML += "\nTHE END or is it?\n";
 }
 
