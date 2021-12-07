6f6d841542378af80bb50f7375549f8617cec348
BoomRocker <boomrocker@gmail.com>
2021-12-06 18:43:26 -0600

    New buttons
    Y=Test

--- script.js
+++ script.js
@@ -3,12 +3,16 @@
 	var countV=0
 	var countH=0
 	
-	// RESET
+	var countVStart=14
+	
+// RESET
 	document.getElementById("resetAll").onclick=function(){
 		window.location.href = window.location.href;
 	}
 
-	
+//START HEALTH
+	document.getElementById("villainTotalStart").innerHTML=countVStart;
+
 		
 //SHIELD START
 	document.getElementById("shieldAdd").onclick=function(){
@@ -43,8 +47,31 @@
 		   
 	   }
 	}
+	
+	document.getElementById("villainAdd5").onclick=function(){
+		countV=countV+5;
+		document.getElementById("villainDamage").innerHTML=countV;
+	  	  if (countV > 0 ){
+			count=Math.max(0, count-1);
+			document.getElementById("damageTotaler").innerHTML=count;
+	   } else {
+		   
+	   }
+	}
+	
 	document.getElementById("villainSubtract").onclick=function(){
 		countV=Math.max(0, countV-1)
+		document.getElementById("villainDamage").innerHTML=countV;
+	  
+	  if (countV >= 0){
+		  countFloat=Math.max(0,countA+countP-countV);
+		  count=countFloat;
+		 document.getElementById("damageTotaler").innerHTML=Math.max(0, count);
+		}
+	}
+	
+	document.getElementById("villainSubtract5").onclick=function(){
+		countV=Math.max(0, countV-5)
 		document.getElementById("villainDamage").innerHTML=countV;
 	  
 	  if (countV >= 0){
